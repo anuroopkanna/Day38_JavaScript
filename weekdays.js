aa72216daa73bfdef4a08fb53b678498a893fa30
anuroopkanna <anuroopindia16@gmail.com>
2023-04-20 04:29:46 +0530

    added:weekdays

---
+++ weekdays.js
@@ -0,0 +1,28 @@
+let num=6;
+if(num==1){
+    console.log("Sunday");
+
+}
+else if(num==2)
+{
+    console.log("Monday");
+}else if(num==3)
+
+{
+    console.log("Tuesday")
+}
+if(num==4){
+    console.log("Wednesday");
+}
+else if(num==5)
+{
+    console.log("Thursday");
+}
+else if(num==6)
+{
+    console.log("Friday");
+}
+else
+{
+console.log("Saturday");
+}
