1b780203651fc004cfda553ed23739a4484e9144
anuroopkanna <anuroopindia16@gmail.com>
2023-04-20 05:18:47 +0530

    added:unittowordscase

---
+++ numtounits.js
@@ -0,0 +1,24 @@
+// var num =prompt("Enter the number:");
+// num=parseInt(num);
+// var output="";
+let num=100;
+switch(num){
+    case 1:
+        console.log("unit");
+        break;
+        case 10:
+            console.log("ten");
+            break;
+            case 100:
+                console.log("Hundred");
+                break;
+                case 1000:
+                    console.log("Thousand");
+                    break;
+                    case 10000:
+                        console.log("Ten thousand");
+                        break;
+                        case 100000:
+                            console.log("One lakh");
+                            break;
+}
